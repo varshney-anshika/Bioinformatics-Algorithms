43d97d631987c9173ef99f7dc6379adaabb0ef9b
Anshika Varshney <anshika.varshney01@gmail.com>
2023-01-31 15:34:45 +0530

    Main

--- 4_Pattern_Matching.py
+++ 4_Pattern_Matching.py
@@ -3,3 +3,9 @@
 # Input: Strings Pattern and Genome.
 # Output: All starting positions in Genome where Pattern appears as a sub-string.
 
+def find_occurrences(text, pattern):
+    occurrences = []
+    for i in range(len(text) - len(pattern) + 1):
+        if text[i:i + len(pattern)] == pattern:
+            occurrences.append(i)
+    return occurrences
